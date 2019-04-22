f8d78ad1bb7c93e47f86475807d64ac133cee115
Sunghoon Ahn <ahnsunghoon0215@gmail.com>
2019-04-21 20:15:25 -0400

    Updated javascript files

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,44 @@
-import axios from 'axios';
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader} from './views/base';
 
-async function getResults(query) {
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const key = '83cd87cd6a96be9be45079b605581dff'; 
-    try {
-        const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-        console.log(res);    
-    } catch (error) {
-        console.log(error);
+
+
+/** Global state of the app 
+ * -Search object
+ * -current recipe object
+ * -Shopping list object
+ * -Liked recipes
+ */
+const state = {};
+
+const controlSearch = async () => {
+    // 1) Get query from view
+    const query = searchView.getInput();
+    console.log(query);
+    
+    if (query) {
+        // 2) New search object and add to state
+        state.search = new Search(query);
+
+        // 3) Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5) Render results on UI
+        clearLoader();
+        searchView.renderResults(state.search.result);
     }
-   
+
 }
 
-getResults('pizza');
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+
+});
+
---
+++ src/js/models/Search.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const key = '83cd87cd6a96be9be45079b605581dff'; 
+        try {
+            const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            
+        } catch (error) {
+            alert(error);
+        }
+   
+    }
+
+}
